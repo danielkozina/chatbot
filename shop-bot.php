e3ed6a49c204229cffc61b79b830386a86ecc2fa
Daniel Kozina <danielkozina@innowator.studio>
2019-10-08 00:30:23 +0200

    Fix

--- shop-bot.php
+++ shop-bot.php
@@ -24,19 +24,36 @@ use Phpml\Tokenization\NGramTokenizer;
 //     ['http://webcrafters.studio/index.php/produkt/sklep-internetowy-gotowy-szablon','http://webcrafters.studio/index.php/produkt/sklep-internetowy-autorska-grafika']
 // );
 
+// czyli pobieranie danych nie ma zadnego znaczenia
+
 $dataset = new FilesDataset('data/webcrafters.studio');
 // var_dump($dataset);
-$split = new StratifiedRandomSplit($dataset, 0.5);
+$split = new StratifiedRandomSplit($dataset, 0.2); // wtedy po 10 próbek z każdej kategorii i 8 wykorzystujemy do uczenia a 2 do walidacji
 // var_dump($split);
 $pipeline = new Pipeline([
     new TokenCountVectorizer($tokenizer = new NGramTokenizer(1, 3), new Polish()),
     new TfIdfTransformer()
-], new SVC(Kernel::LINEAR));
+], new SVC());
+
 // var_dump($pipeline);
 
+//predict propability i zwraca tablice gdzie jest przedzial jak bardzo jest pewny odpowiedzi SVM czyli Pipeline nie ma tego // 
+$start = microtime(true);
 $pipeline->train($split->getTrainSamples(), $split->getTrainLabels());
+$stop = microtime(true);
+// trenowanie ma sens tylko jesli zmieni się zawartość katalogu - trenowanie powinno być offline czyli po prostu plik wyczony wrzumay na serwer i na podsatwei jego odpowiadać
+// można tu policzyć accuracy żeby dowiedzieć się jak on się wytrenował choćby ogólnie
+// confusion matrix sprawdzić / chodzi o to że ma przewidziane i prawidzwe czyli zestawienie dwoch wartosci i mozemy przewidzieć / sprawdzenie gdzie się pomylił
+// accuracy na 90% da już odpowiednią ocenę, można wyswietlać przy trenowaniu
+// głównym problemem jest to accuracy dokładnie
+
 $predicted = $pipeline->predict($split->getTestSamples());
 // var_dump($predicted);
 
+echo 'Train: ' . round($stop - $start, 4) . 's'. PHP_EOL;
+echo 'Estimator: ' . get_class($pipeline->getEstimator()) . PHP_EOL;
+echo 'Tokenizer: ' . get_class($tokenizer) . PHP_EOL;
+echo 'Accuracy: ' . Accuracy::score($split->getTestLabels(), $predicted);
+
 $modelManager = new ModelManager();
-$modelManager->saveToFile($pipeline,'model/shop-model.phpml');
+$modelManager->saveToFile($pipeline,'model/shop-model.phpml'); // zapisuje się model
--- shop-restore.php
+++ shop-restore.php
@@ -10,10 +10,17 @@ include 'vendor/autoload.php';
 $modelManager = new ModelManager();
 $model = $modelManager->restoreFromFile('model/shop-model.phpml');
 
-// $text = "Zakładanie własnego sklepu internetowego to najlepsza z możliwych opcji";
-$text = "Autorska grafika strony internetowej";
-// $text = "najlepszy sposób na pozyskanie nowych klientów";
+$text = "Chciałbym zbudowałać własny sklep internetowy."; // wordpress
+$text = "Chciałbym zbudowałać własną stronę internetową."; // wordpress
+$text = "Dzień dobry, chciałbym zbudowałać własną stronę internetową."; // wordpress
+$text = "Dzień dobry, czy jest możliwe skorzystanie z gotowego motywu graficznego?"; // wordpress
+
+$text = "Dzień dobry, interesuje mnie dobra reklama w interneci i pozyskanie nowych klientów."; // marketing
+$text = "Dzień dobry, interesuje mnie Pozycjonowanie w wyszukiwarce Google i wysoka pozycja w wynikach wyszukiwania."; // marketing
+$text = "Dzień dobry, mam pytanie o płatne reklamy. Czy pozwalają na skuteczną reklamę w sieci. Zastanawiam sie jakie jest idealne narzędzie do pozycjonowania."; // marketing
 
 $predicted = $model->predict([$text]);
+
+// za mało próbek do uczenia się dlatego może odpowiadać ostatni rekordem
 
 var_dump($predicted);
